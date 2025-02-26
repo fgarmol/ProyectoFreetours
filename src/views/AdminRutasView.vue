281a8a0f330dd44000a6de44169e5b9063b31b2b
Ruben <fgarmol2808@g.educaand.es>
2025-02-26 01:01:32 +0100

    Add dayjs library to package dependencies

--- src/views/AdminRutasView.vue
+++ src/views/AdminRutasView.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { ref, computed, onMounted } from 'vue';
+import dayjs from 'dayjs'; // Asegúrate de tener dayjs instalado
 
 const props = defineProps({
     usuarioAutenticado: {
@@ -11,12 +12,17 @@ const props = defineProps({
 const emit = defineEmits(['sesionIniciada']);
 
 const rutas = ref([]);
-const paginaActual = ref(1);
-const itemsPorPagina = 10;
+const activeTab = ref('pendientes');
 const guiasDisponibles = ref({});
 const guiasDisponiblesModal = ref([]);
 const sortKey = ref('');
 const sortOrder = ref(1); // 1 for ascending, -1 for descending
+
+const showModal = ref(false);
+const paginaActual = ref(1);
+const totalPaginas = ref(1);
+const selectedRuta = ref(null);
+const nuevaFecha = ref('');
 
 function showAlert(message, isSuccess = false) {
     const alert = document.getElementById('alert');
@@ -82,13 +88,11 @@ function asignarGuia(ruta) {
         .catch(error => showAlert(`Error al asignar guía: ${error.message}`));
 }
 
-
 function obtenerGuias(ruta) {
     const disponibles = guiasDisponibles.value[ruta.id] || [];
     if (ruta.guia_id) {
         const existe = disponibles.find(g => g.id === ruta.guia_id);
         if (!existe) {
-
             return [{ id: ruta.guia_id, nombre: ruta.guia_nombre || 'Sin nombre' }, ...disponibles];
         }
     }
@@ -99,103 +103,13 @@ onMounted(() => {
     cargarRutas();
 });
 
-const rutasPaginadas = computed(() => {
-    const inicio = (paginaActual.value - 1) * itemsPorPagina;
-    return rutas.value.slice(inicio, inicio + itemsPorPagina);
+const rutasPendientes = computed(() => {
+    return rutas.value.filter(ruta => dayjs(ruta.fecha).isAfter(dayjs()));
 });
 
-const totalPaginas = computed(() => {
-    return Math.ceil(rutas.value.length / itemsPorPagina);
+const rutasPasadas = computed(() => {
+    return rutas.value.filter(ruta => dayjs(ruta.fecha).isBefore(dayjs()));
 });
-
-function paginaSiguiente() {
-    if (paginaActual.value < totalPaginas.value) {
-        paginaActual.value++;
-    }
-}
-
-function paginaAnterior() {
-    if (paginaActual.value > 1) {
-        paginaActual.value--;
-    }
-}
-
-const showModal = ref(false);
-const selectedRuta = ref(null);
-const nuevaFecha = ref('');
-
-function openModal(ruta) {
-    selectedRuta.value = ruta;
-    nuevaFecha.value = ruta.fecha;
-    showModal.value = true;
-    cargarGuia(nuevaFecha.value, ruta.id);
-}
-
-function closeModal() {
-    showModal.value = false;
-    selectedRuta.value = null;
-    nuevaFecha.value = '';
-    guiasDisponiblesModal.value = [];
-}
-
-
-
-function eliminarRuta(id) {
-    if (!confirm('¿Estás seguro de que deseas eliminar esta ruta?')) {
-        return;
-    }
-    fetch(`http://localhost/APIFreetours/api.php/rutas?id=${id}`, {
-        method: 'DELETE',
-    })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(`Error ${response.status}: ${response.statusText}`);
-            }
-            return response.json();
-        })
-        .then(responseData => {
-            showAlert('Ruta eliminada exitosamente', true);
-            cargarRutas();
-        })
-        .catch(error => showAlert(`Error al eliminar la ruta: ${error.message}`, false));
-}
-
-
-function duplicarRuta() {
-    if (selectedRuta.value) {
-        const data = {
-            titulo: selectedRuta.value.titulo,
-            localidad: selectedRuta.value.localidad,
-            descripcion: selectedRuta.value.descripcion,
-            foto: selectedRuta.value.foto,
-            fecha: nuevaFecha.value,
-            hora: selectedRuta.value.hora,
-            latitud: selectedRuta.value.latitud,
-            longitud: selectedRuta.value.longitud,
-            guia_id: selectedRuta.value.guia_id
-        };
-
-        fetch("http://localhost/APIFreetours/api.php/rutas", {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data)
-        })
-            .then(response => {
-                if (!response.ok) throw new Error(`Error ${response.status}: ${response.statusText}`);
-                return response.json();
-            })
-            .then(() => {
-                showAlert('Ruta duplicada correctamente', true);
-                cargarRutas();
-                closeModal();
-            })
-            .catch(error => showAlert(`Error al duplicar ruta: ${error.message}`));
-    }
-}
-
-const totalRutas = computed(() => rutas.value.length);
 
 function sortBy(key) {
     if (sortKey.value === key) {
@@ -211,6 +125,33 @@ function sortBy(key) {
     });
 }
 
+function openModal(ruta) {
+    selectedRuta.value = ruta;
+    showModal.value = true;
+}
+
+function closeModal() {
+    showModal.value = false;
+}
+
+function duplicarRuta() {
+    // Implementar la lógica para duplicar la ruta
+    closeModal();
+}
+
+function paginaAnterior() {
+    if (paginaActual.value > 1) {
+        paginaActual.value--;
+        cargarRutas();
+    }
+}
+
+function paginaSiguiente() {
+    if (paginaActual.value < totalPaginas.value) {
+        paginaActual.value++;
+        cargarRutas();
+    }
+}
 </script>
 
 <template>
@@ -222,46 +163,101 @@ function sortBy(key) {
         </div>
 
         <div class="counters d-flex justify-content-end">
-            <span class="fw-bold">Total de rutas: {{ totalRutas }}</span>
+            <span class="fw-bold">Total de rutas: {{ rutas.length }}</span>
         </div>
-        <table class="table">
-            <thead>
-                <tr>
-                    <th @click="sortBy('titulo')">Título</th>
-                    <th @click="sortBy('localidad')">Localidad</th>
-                    <th @click="sortBy('descripcion')">Descripción</th>
-                    <th @click="sortBy('fecha')">Fecha</th>
-                    <th @click="sortBy('hora')">Hora</th>
-                    <th @click="sortBy('latitud')">Latitud</th>
-                    <th @click="sortBy('longitud')">Longitud</th>
-                    <th>Guía</th>
-                    <th>Acciones</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for="ruta in rutasPaginadas" :key="ruta.id">
-                    <td>{{ ruta.titulo }}</td>
-                    <td>{{ ruta.localidad }}</td>
-                    <td>{{ ruta.descripcion }}</td>
-                    <td>{{ ruta.fecha }}</td>
-                    <td>{{ ruta.hora }}</td>
-                    <td>{{ ruta.latitud }}</td>
-                    <td>{{ ruta.longitud }}</td>
-                    <td>
-                        <select v-model="ruta.guia_id" @change="asignarGuia(ruta)">
-                            <option disabled>Seleccionar guía</option>
-                            <option v-for="guia in obtenerGuias(ruta)" :key="guia.id" :value="guia.id">
-                                {{ guia.nombre }}
-                            </option>
-                        </select>
-                    </td>
-                    <td>
-                        <button @click="eliminarRuta(ruta.id)" class="btn btn-danger">Eliminar</button>
-                        <button @click="openModal(ruta)" class="btn btn-secondary">Duplicar</button>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
+
+        <ul class="nav nav-tabs">
+            <li class="nav-item">
+                <a class="nav-link" :class="{ active: activeTab === 'pendientes' }" @click="activeTab = 'pendientes'">Rutas Pendientes</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" :class="{ active: activeTab === 'pasadas' }" @click="activeTab = 'pasadas'">Rutas Pasadas</a>
+            </li>
+        </ul>
+
+        <div class="tab-content">
+            <div v-if="activeTab === 'pendientes'" class="tab-pane active">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th @click="sortBy('titulo')">Título</th>
+                            <th @click="sortBy('localidad')">Localidad</th>
+                            <th @click="sortBy('descripcion')">Descripción</th>
+                            <th @click="sortBy('fecha')">Fecha</th>
+                            <th @click="sortBy('hora')">Hora</th>
+                            <th @click="sortBy('latitud')">Latitud</th>
+                            <th @click="sortBy('longitud')">Longitud</th>
+                            <th>Guía</th>
+                            <th>Acciones</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="ruta in rutasPendientes" :key="ruta.id">
+                            <td>{{ ruta.titulo }}</td>
+                            <td>{{ ruta.localidad }}</td>
+                            <td>{{ ruta.descripcion }}</td>
+                            <td>{{ ruta.fecha }}</td>
+                            <td>{{ ruta.hora }}</td>
+                            <td>{{ ruta.latitud }}</td>
+                            <td>{{ ruta.longitud }}</td>
+                            <td>
+                                <select v-model="ruta.guia_id" @change="asignarGuia(ruta)">
+                                    <option disabled>Seleccionar guía</option>
+                                    <option v-for="guia in obtenerGuias(ruta)" :key="guia.id" :value="guia.id">
+                                        {{ guia.nombre }}
+                                    </option>
+                                </select>
+                            </td>
+                            <td>
+                                <button @click="eliminarRuta(ruta.id)" class="btn btn-danger">Eliminar</button>
+                                <button @click="openModal(ruta)" class="btn btn-secondary">Duplicar</button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div v-if="activeTab === 'pasadas'" class="tab-pane active">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th @click="sortBy('titulo')">Título</th>
+                            <th @click="sortBy('localidad')">Localidad</th>
+                            <th @click="sortBy('descripcion')">Descripción</th>
+                            <th @click="sortBy('fecha')">Fecha</th>
+                            <th @click="sortBy('hora')">Hora</th>
+                            <th @click="sortBy('latitud')">Latitud</th>
+                            <th @click="sortBy('longitud')">Longitud</th>
+                            <th>Guía</th>
+                            <th>Acciones</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="ruta in rutasPasadas" :key="ruta.id">
+                            <td>{{ ruta.titulo }}</td>
+                            <td>{{ ruta.localidad }}</td>
+                            <td>{{ ruta.descripcion }}</td>
+                            <td>{{ ruta.fecha }}</td>
+                            <td>{{ ruta.hora }}</td>
+                            <td>{{ ruta.latitud }}</td>
+                            <td>{{ ruta.longitud }}</td>
+                            <td>
+                                <select v-model="ruta.guia_id" @change="asignarGuia(ruta)">
+                                    <option disabled>Seleccionar guía</option>
+                                    <option v-for="guia in obtenerGuias(ruta)" :key="guia.id" :value="guia.id">
+                                        {{ guia.nombre }}
+                                    </option>
+                                </select>
+                            </td>
+                            <td>
+                                <button @click="eliminarRuta(ruta.id)" class="btn btn-danger">Eliminar</button>
+                                <button @click="openModal(ruta)" class="btn btn-secondary">Duplicar</button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
         <div class="pagination">
             <button @click="paginaAnterior" :disabled="paginaActual === 1">Anterior</button>
             <span>Página {{ paginaActual }} de {{ totalPaginas }}</span>
